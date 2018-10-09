c347f4a0441b939b81f622dbb56717b037e2a154
Javi Cancela <cancelajavi@gmail.com>
2018-10-09 20:36:33 +0200

    scaffolding initialization

--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CoreComponent } from './core.component';
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [CoreComponent],
-  exports: [CoreComponent]
 })
 export class CoreModule { }
